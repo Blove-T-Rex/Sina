22012bd49076142c691f02030b24733bf95d35ae
mini-bear <591128914@qq.com>
2018-07-23 09:49:27 +0800

    modifyComponentLove&Star

--- src/components/Love/index.js
+++ src/components/Love/index.js
@@ -1,21 +1,31 @@
 import React,{Component} from "react";
 import "./index.css";
 import axios from "axios";
-import { PullToRefresh } from 'antd-mobile';
-import 'antd-mobile/dist/antd-mobile.css'; 
-import Header from "../Header"
+import { PullToRefresh, Button } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
+
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie'; 
 
 class Love extends Component{
+	static propTypes = {
+	   cookies: instanceOf(Cookies).isRequired
+	 };
 
 	constructor(props){
 		super(props);
+		const { cookies } = props;
 		this.state = {
 			datalist:[],
 			 refreshing: false,
 		     down: true,
 		     height: document.documentElement.clientHeight,
 		     data: [],
-		     page:1
+		     page:1,
+		     _T_WM: cookies.get('_T_WM') || '83690b72c7a881facde94807d6b2eef1',
+		     WEIBOCN_FROM:cookies.get('WEIBOCN_FROM') || '1110006030',
+		     MLOGIN:cookies.get('MLOGIN') || '0',
+		     M_WEIBOCN_PARAMS:cookies.get('M_WEIBOCN_PARAMS') || 'oid%3D4263231486518665%26luicode%3D10000011%26lfid%3D102803_ctg1_1988_-_ctg1_1988%26uicode%3D20000174'
 		};
 	}
 
@@ -23,7 +33,6 @@ class Love extends Component{
 	render(){
 		return(
 			<div id="love">
-			<Header></Header>
 				<ul>
 					
 					<PullToRefresh
@@ -37,9 +46,15 @@ class Love extends Component{
 					        direction={this.state.down ? 'down' : 'up'}
 					        refreshing={this.state.refreshing}
 					        onRefresh={() => {
+							  const { cookies } = this.props;
+   							  cookies.set('_T_WM', this.state._T_WM, { path: '/' });
+   							  cookies.set('WEIBOCN_FROM', this.state.WEIBOCN_FROM, { path: '/' });
+   							  cookies.set('MLOGIN', this.state.MLOGIN, { path: '/' });
+   							  cookies.set('M_WEIBOCN_PARAMS', this.state.M_WEIBOCN_PARAMS, { path: '/' });
+
 					          this.setState({ 
 					          	refreshing: true,
-					          	page:this.state.page + 1 
+					          	page:this.state.page + 1
 					          });
 					          axios.get(`/api/container/getIndex?containerid=102803_ctg1_1988_-_ctg1_1988&openApp=0&since_id=${this.state.page}`).then(res=>{
 					          	console.log(res.data.data.cards)
@@ -53,12 +68,13 @@ class Love extends Component{
 					 >
 					{
 						this.state.datalist.map(item=>
+		
 							<li key={item.mblog.id} onClick={this.handleClick.bind(this,item.mblog.id)}>
 								<div className="card-weibo">
 									<div className="card-wrap">
 										<header>
 											<div className="left">
-												<img src={item.mblog.user.avatar_hd} alt={item.mblog.user.avatar_hd}/>
+												<img src={item.mblog.user.avatar_hd}/>
 											</div>
 											<div className="center">
 												<div className="text">
@@ -93,7 +109,7 @@ class Love extends Component{
 													{
 														item.mblog.pics&&item.mblog.pics.map(pic=>
 															<li key={pic.url}>
-																<img src={pic.url} alt={pic.url}/>
+																<img src={pic.url}/>
 															</li>
 														)
 													}
@@ -122,6 +138,7 @@ class Love extends Component{
 									</div>
 								</div>
 							</li>
+						
 						)
 					}
 					</PullToRefresh>
@@ -145,4 +162,4 @@ class Love extends Component{
 	
 }
 
-export default Love
+export default withCookies(Love);
--- src/components/Star/index.js
+++ src/components/Star/index.js
@@ -1,21 +1,31 @@
 import React,{Component} from "react";
 import "./index.css";
 import axios from "axios";
-import { PullToRefresh } from 'antd-mobile';
-import 'antd-mobile/dist/antd-mobile.css'; 
-import Header from "../Header"
+import { PullToRefresh, Button } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
 
-class Love extends Component{
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';  
+
+class Star extends Component{
+	static propTypes = {
+	   cookies: instanceOf(Cookies).isRequired
+	 };
 
 	constructor(props){
 		super(props);
+		const { cookies } = props;
 		this.state = {
 			datalist:[],
 			 refreshing: false,
 		     down: true,
 		     height: document.documentElement.clientHeight,
 		     data: [],
-		     page:1
+		     page:1,
+		      _T_WM: cookies.get('_T_WM') || '83690b72c7a881facde94807d6b2eef1',
+		     WEIBOCN_FROM:cookies.get('WEIBOCN_FROM') || '1110006030',
+		     MLOGIN:cookies.get('MLOGIN') || '0',
+		     M_WEIBOCN_PARAMS:cookies.get('M_WEIBOCN_PARAMS') || 'luicode%3D10000011%26lfid%3D102803_ctg1_4288_-_ctg1_4288%26fid%3D102803_ctg1_4288_-_ctg1_4288%26uicode%3D10000011'
 		};
 	}
 
@@ -23,7 +33,6 @@ class Love extends Component{
 	render(){
 		return(
 			<div id="star">
-			<Header></Header>
 				<ul>
 					
 					<PullToRefresh
@@ -37,6 +46,12 @@ class Love extends Component{
 					        direction={this.state.down ? 'down' : 'up'}
 					        refreshing={this.state.refreshing}
 					        onRefresh={() => {
+							  const { cookies } = this.props;
+   							  cookies.set('_T_WM', this.state._T_WM, { path: '/' });
+   							  cookies.set('WEIBOCN_FROM', this.state.WEIBOCN_FROM, { path: '/' });
+   							  cookies.set('MLOGIN', this.state.MLOGIN, { path: '/' });
+   							  cookies.set('M_WEIBOCN_PARAMS', this.state.M_WEIBOCN_PARAMS, { path: '/' });
+
 					          this.setState({ 
 					          	refreshing: true,
 					          	page:this.state.page + 1 
@@ -58,7 +73,7 @@ class Love extends Component{
 									<div className="card-wrap">
 										<header>
 											<div className="left">
-												<img src={item.mblog.user.avatar_hd} alt={item.mblog.user.avatar_hd}/>
+												<img src={item.mblog.user.avatar_hd}/>
 											</div>
 											<div className="center">
 												<div className="text">
@@ -93,7 +108,7 @@ class Love extends Component{
 													{
 														item.mblog.pics&&item.mblog.pics.map(pic=>
 															<li key={pic.url}>
-																<img src={pic.url} alt={pic.url}/>
+																<img src={pic.url}/>
 															</li>
 														)
 													}
@@ -145,4 +160,4 @@ class Love extends Component{
 	
 }
 
-export default Love
+export default withCookies(Star);
